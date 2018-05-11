574d07ca1dc69e70e74919e0f129131fe7768295
ragibhasan894 <ragibhasan894@gmail.com>
2018-05-11 16:34:00 +0600

    modified TopicsController.php and add.ctp

--- app/Controller/TopicsController.php
+++ app/Controller/TopicsController.php
@@ -4,7 +4,10 @@
 	class TopicsController extends AppController {
 
 		public function add() {
-			
+			if($this->request->is('post')) {
+				$this->Topic->create();
+				$this->Topic->save($this->request->data);
+			}
 		}
 
 	}
